cf5aea592d46b2f34e52498b3457930b4bd759dc
meteor-a <61750320+meteor-a@users.noreply.github.com>
2021-08-18 11:52:49 +0300

    Add files via upload

---
+++ debug/moc_calculatorwindow.cpp
@@ -0,0 +1,271 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'calculatorwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../calculatorwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'calculatorwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.14.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_CalculatorWindow_t {
+    QByteArrayData data[35];
+    char stringdata0[1125];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_CalculatorWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_CalculatorWindow_t qt_meta_stringdata_CalculatorWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 16), // "CalculatorWindow"
+QT_MOC_LITERAL(1, 17, 30), // "on_pushButton_number_0_clicked"
+QT_MOC_LITERAL(2, 48, 0), // ""
+QT_MOC_LITERAL(3, 49, 34), // "on_pushButton_action_comma_cl..."
+QT_MOC_LITERAL(4, 84, 34), // "on_pushButton_action_point_cl..."
+QT_MOC_LITERAL(5, 119, 33), // "on_pushButton_action_plus_cli..."
+QT_MOC_LITERAL(6, 153, 35), // "on_pushButton_action_result_c..."
+QT_MOC_LITERAL(7, 189, 30), // "on_pushButton_number_1_clicked"
+QT_MOC_LITERAL(8, 220, 30), // "on_pushButton_number_2_clicked"
+QT_MOC_LITERAL(9, 251, 30), // "on_pushButton_number_3_clicked"
+QT_MOC_LITERAL(10, 282, 34), // "on_pushButton_action_minus_cl..."
+QT_MOC_LITERAL(11, 317, 30), // "on_pushButton_number_4_clicked"
+QT_MOC_LITERAL(12, 348, 30), // "on_pushButton_number_5_clicked"
+QT_MOC_LITERAL(13, 379, 30), // "on_pushButton_number_6_clicked"
+QT_MOC_LITERAL(14, 410, 30), // "on_pushButton_number_7_clicked"
+QT_MOC_LITERAL(15, 441, 30), // "on_pushButton_number_8_clicked"
+QT_MOC_LITERAL(16, 472, 30), // "on_pushButton_number_9_clicked"
+QT_MOC_LITERAL(17, 503, 35), // "on_pushButton_action_divide_c..."
+QT_MOC_LITERAL(18, 539, 37), // "on_pushButton_action_multiply..."
+QT_MOC_LITERAL(19, 577, 47), // "on_pushButton_action_open_rou..."
+QT_MOC_LITERAL(20, 625, 48), // "on_pushButton_action_close_ro..."
+QT_MOC_LITERAL(21, 674, 48), // "on_pushButton_action_open_squ..."
+QT_MOC_LITERAL(22, 723, 49), // "on_pushButton_action_close_sq..."
+QT_MOC_LITERAL(23, 773, 33), // "on_pushButton_clear_input_cli..."
+QT_MOC_LITERAL(24, 807, 32), // "on_pushButton_action_sin_clicked"
+QT_MOC_LITERAL(25, 840, 32), // "on_pushButton_action_cos_clicked"
+QT_MOC_LITERAL(26, 873, 35), // "on_pushButton_action_degree_c..."
+QT_MOC_LITERAL(27, 909, 31), // "on_pushButton_action_ln_clicked"
+QT_MOC_LITERAL(28, 941, 32), // "on_pushButton_action_log_clicked"
+QT_MOC_LITERAL(29, 974, 31), // "on_pushButton_action_Pi_clicked"
+QT_MOC_LITERAL(30, 1006, 30), // "on_pushButton_action_e_clicked"
+QT_MOC_LITERAL(31, 1037, 13), // "set_variables"
+QT_MOC_LITERAL(32, 1051, 20), // "map<QString,QString>"
+QT_MOC_LITERAL(33, 1072, 15), // "vector<QString>"
+QT_MOC_LITERAL(34, 1088, 36) // "on_pushButton_action_balance_..."
+
+    },
+    "CalculatorWindow\0on_pushButton_number_0_clicked\0"
+    "\0on_pushButton_action_comma_clicked\0"
+    "on_pushButton_action_point_clicked\0"
+    "on_pushButton_action_plus_clicked\0"
+    "on_pushButton_action_result_clicked\0"
+    "on_pushButton_number_1_clicked\0"
+    "on_pushButton_number_2_clicked\0"
+    "on_pushButton_number_3_clicked\0"
+    "on_pushButton_action_minus_clicked\0"
+    "on_pushButton_number_4_clicked\0"
+    "on_pushButton_number_5_clicked\0"
+    "on_pushButton_number_6_clicked\0"
+    "on_pushButton_number_7_clicked\0"
+    "on_pushButton_number_8_clicked\0"
+    "on_pushButton_number_9_clicked\0"
+    "on_pushButton_action_divide_clicked\0"
+    "on_pushButton_action_multiply_clicked\0"
+    "on_pushButton_action_open_round_bracket_clicked\0"
+    "on_pushButton_action_close_round_bracket_clicked\0"
+    "on_pushButton_action_open_square_bracket_clicked\0"
+    "on_pushButton_action_close_square_bracket_clicked\0"
+    "on_pushButton_clear_input_clicked\0"
+    "on_pushButton_action_sin_clicked\0"
+    "on_pushButton_action_cos_clicked\0"
+    "on_pushButton_action_degree_clicked\0"
+    "on_pushButton_action_ln_clicked\0"
+    "on_pushButton_action_log_clicked\0"
+    "on_pushButton_action_Pi_clicked\0"
+    "on_pushButton_action_e_clicked\0"
+    "set_variables\0map<QString,QString>\0"
+    "vector<QString>\0on_pushButton_action_balance_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_CalculatorWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      31,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  169,    2, 0x08 /* Private */,
+       3,    0,  170,    2, 0x08 /* Private */,
+       4,    0,  171,    2, 0x08 /* Private */,
+       5,    0,  172,    2, 0x08 /* Private */,
+       6,    0,  173,    2, 0x08 /* Private */,
+       7,    0,  174,    2, 0x08 /* Private */,
+       8,    0,  175,    2, 0x08 /* Private */,
+       9,    0,  176,    2, 0x08 /* Private */,
+      10,    0,  177,    2, 0x08 /* Private */,
+      11,    0,  178,    2, 0x08 /* Private */,
+      12,    0,  179,    2, 0x08 /* Private */,
+      13,    0,  180,    2, 0x08 /* Private */,
+      14,    0,  181,    2, 0x08 /* Private */,
+      15,    0,  182,    2, 0x08 /* Private */,
+      16,    0,  183,    2, 0x08 /* Private */,
+      17,    0,  184,    2, 0x08 /* Private */,
+      18,    0,  185,    2, 0x08 /* Private */,
+      19,    0,  186,    2, 0x08 /* Private */,
+      20,    0,  187,    2, 0x08 /* Private */,
+      21,    0,  188,    2, 0x08 /* Private */,
+      22,    0,  189,    2, 0x08 /* Private */,
+      23,    0,  190,    2, 0x08 /* Private */,
+      24,    0,  191,    2, 0x08 /* Private */,
+      25,    0,  192,    2, 0x08 /* Private */,
+      26,    0,  193,    2, 0x08 /* Private */,
+      27,    0,  194,    2, 0x08 /* Private */,
+      28,    0,  195,    2, 0x08 /* Private */,
+      29,    0,  196,    2, 0x08 /* Private */,
+      30,    0,  197,    2, 0x08 /* Private */,
+      31,    1,  198,    2, 0x08 /* Private */,
+      34,    0,  201,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    0x80000000 | 32, 0x80000000 | 33,    2,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void CalculatorWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<CalculatorWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_pushButton_number_0_clicked(); break;
+        case 1: _t->on_pushButton_action_comma_clicked(); break;
+        case 2: _t->on_pushButton_action_point_clicked(); break;
+        case 3: _t->on_pushButton_action_plus_clicked(); break;
+        case 4: _t->on_pushButton_action_result_clicked(); break;
+        case 5: _t->on_pushButton_number_1_clicked(); break;
+        case 6: _t->on_pushButton_number_2_clicked(); break;
+        case 7: _t->on_pushButton_number_3_clicked(); break;
+        case 8: _t->on_pushButton_action_minus_clicked(); break;
+        case 9: _t->on_pushButton_number_4_clicked(); break;
+        case 10: _t->on_pushButton_number_5_clicked(); break;
+        case 11: _t->on_pushButton_number_6_clicked(); break;
+        case 12: _t->on_pushButton_number_7_clicked(); break;
+        case 13: _t->on_pushButton_number_8_clicked(); break;
+        case 14: _t->on_pushButton_number_9_clicked(); break;
+        case 15: _t->on_pushButton_action_divide_clicked(); break;
+        case 16: _t->on_pushButton_action_multiply_clicked(); break;
+        case 17: _t->on_pushButton_action_open_round_bracket_clicked(); break;
+        case 18: _t->on_pushButton_action_close_round_bracket_clicked(); break;
+        case 19: _t->on_pushButton_action_open_square_bracket_clicked(); break;
+        case 20: _t->on_pushButton_action_close_square_bracket_clicked(); break;
+        case 21: _t->on_pushButton_clear_input_clicked(); break;
+        case 22: _t->on_pushButton_action_sin_clicked(); break;
+        case 23: _t->on_pushButton_action_cos_clicked(); break;
+        case 24: _t->on_pushButton_action_degree_clicked(); break;
+        case 25: _t->on_pushButton_action_ln_clicked(); break;
+        case 26: _t->on_pushButton_action_log_clicked(); break;
+        case 27: _t->on_pushButton_action_Pi_clicked(); break;
+        case 28: _t->on_pushButton_action_e_clicked(); break;
+        case 29: { map<QString,QString> _r = _t->set_variables((*reinterpret_cast< vector<QString>(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< map<QString,QString>*>(_a[0]) = std::move(_r); }  break;
+        case 30: _t->on_pushButton_action_balance_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject CalculatorWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CalculatorWindow.data,
+    qt_meta_data_CalculatorWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *CalculatorWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *CalculatorWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CalculatorWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int CalculatorWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 31)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 31;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 31)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 31;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
